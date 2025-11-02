aa02f0ff13030f03fe1a80eba261c8b6f205190d
Tomas Aguilera <t.aguilera@uniandes.edu.co>
2025-11-02 03:06:25 -0500

    Update Main class to test some features

--- src/main/java/grupo5/Main.java
+++ src/main/java/grupo5/Main.java
@@ -3,18 +3,28 @@ package grupo5;
 import grupo5.buzones.BuzonBasico;
 import grupo5.buzones.BuzonEntrada;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Cliente[] clientes = new Cliente[5];
-        BuzonEntrada buzon = new BuzonEntrada(10);
+        BuzonEntrada buzon = new BuzonEntrada(25);
 
         for (int i = 0; i < clientes.length; i++) {
-            clientes[i] = new Cliente(i, buzon);
+            clientes[i] = new Cliente(5, buzon);
             clientes[i].start();
         }
 
+        for (int i = 0; i < clientes.length; i++) {
+            try {
+                clientes[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(buzon.lleno());
 
+        for (int i = 0; i < 25; i++) {
+            Correo correo = buzon.extraer();
+            System.out.println(correo.toString());
+        }
     }
 }
